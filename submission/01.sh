1a220d3c924cac82a93d276844c93994ed54d48d
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2025-03-28 14:19:45 +0100

    test the variable

--- submission/01.sh
+++ submission/01.sh
@@ -1,1 +1,2 @@
 # Create a wallet with the name "btrustwallet".
+bitcoin-cli -regtest -named createwallet wallet_name=btrustwallet 
--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,6 @@
 # Created a SegWit address.
+SEGWIT_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
 # Add funds to the address.
+FUND_ADDRESS=$(bitcoin-cli -regtest generatetoaddress 101 "$SEGWIT_ADDRESS")
 # Return only the Address
+echo "$SEGWIT_ADDRESS"
